42d90986a15dcaecff9bbc2e7490ac98adb09ce4
Karishma Gandhi <kari.g2807@gmail.com>
2024-02-10 12:27:57 -0500

    End of Section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,8 +18,8 @@ export class AccountService {
       map((response: User) =>{
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user))
-          this.currentUserSource.next(user)
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
       })
     )
@@ -40,6 +40,7 @@ return user;
   }
   logout(){
     localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
 
